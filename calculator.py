6a7e706f42a068e2bc2734f573301f675f525367
joaquinlpereyra <joaquinlpereyra@gmail.com>
2017-01-22 17:33:19 -0300

    fixed mathematical error

--- calculator.py
+++ calculator.py
@@ -6,13 +6,13 @@ def _rounding(something):
     return something.quantize(cent, rounding=ROUND_DOWN)
 
 def calculator(price, months, expected_inflation = 2, interest_rate = 0):
-    expected_inflation = _rounding((100 - expected_inflation) / 100)
+    expected_inflation = _rounding((100 + expected_inflation) / 100)
     interest_rate = _rounding((100 + interest_rate) / 100)
     price = _rounding(price) * interest_rate
     official_price = price / months
     last_price_paid = official_price
     real_prices = []
     for n in range(1, months+1):
-        last_price_paid = _rounding(last_price_paid * expected_inflation)
+        last_price_paid = _rounding(last_price_paid / expected_inflation)
         real_prices.append(last_price_paid)
     return real_prices
